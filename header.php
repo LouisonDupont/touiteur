285d12ff91b670451f3eb542b6f8cd2853ef12aa
Louison <dupontlouison@gmail.com>
2021-01-28 12:46:53 +0100

    Création de l'environnement de dev

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Description de ma page">
+    <title>Titre de ma page</title>
+    <link rel="stylesheet" href="assets/css/main.min.css">
+</head>
+<body>
+
+    <header>
+        <h1>test</h1></header>
+      
+    <script src="assets/js/bundle.min.js"></script>

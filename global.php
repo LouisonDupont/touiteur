9ad89ac7b87548de83550902d092967828315687
Louison <dupontlouison@gmail.com>
2021-02-02 17:18:29 +0100

    Finition du style

--- global.php
+++ global.php
@@ -2,40 +2,78 @@
     <div class="container">
         <div class="global_left_content">
             <div class="like_content">
-                <h2>most liked</h2>
+                <h2>Most liked</h2>
                 <article class="like_card">
-                    <p class="like_pseudo">@jean-mi-du-72</p>
                     <p class="like_message">Il était une fois ...</p>
-                    <p class="like_number">70</p>
+                    <p class="like_pseudo">@jean-mi-du-72</p>
+                    <div class="like_number">
+                        <img src="src/img/number.svg" alt="">
+                        <p class="number_total">70</p>
+                    </div>
                 </article>
                 <article class="like_card">
-                    <p class="like_pseudo">@jean-mi-du-72</p>
                     <p class="like_message">Il était une fois ...</p>
-                    <p class="like_number">70</p>
-                </article>
-                <article class="like_card">
                     <p class="like_pseudo">@jean-mi-du-72</p>
-                    <p class="like_message">Il était une fois ...</p>
-                    <p class="like_number">70</p>
-                </article><article class="like_card">
-                    <p class="like_pseudo">@jean-mi-du-72</p>
-                    <p class="like_message">Il était une fois ...</p>
-                    <p class="like_number">70</p>
-                </article>
-                <article class="like_card">
-                    <p class="like_pseudo">@jean-mi-du-72</p>
-                    <p class="like_message">Il était une fois ...</p>
-                    <p class="like_number">70</p>
+                    <div class="like_number">
+                        <img src="src/img/number.svg" alt="">
+                        <p class="number_total">70</p>
+                    </div>
                 </article>
             </div>
             <div class="author_content">
+                <h2>Best author</h2>
+                <div class="author_box">
+                    <article class="author_card">
+                        <p class="author_pseudo">@JC-du-41</p>
+                    </article>
+                    <article class="author_card">
+                        <p class="author_pseudo">@JC-du-41</p>
+                    </article>
+                    <article class="author_card">
+                        <p class="author_pseudo">@JC-du-41</p>
+                    </article>
+                    <article class="author_card">
+                        <p class="author_pseudo">@JC-du-41</p>
+                    </article>
+                    <article class="author_card">
+                        <p class="author_pseudo">@JC-du-41</p>
+                    </article>
+                    <article class="author_card">
+                        <p class="author_pseudo">@JC-du-41</p>
+                    </article>
+                </div>
                 
             </div>
             <div class="words_content">
+                <h2>Words champion</h2>
+                    <div class="words_box">
+                        <article class="words_card">
+                            <p class="words_display">Super</p>
+                        </article>
+                        <article class="words_card">
+                            <p class="words_display">Génial</p>
+                        </article>
+                        <article class="words_card">
+                            <p class="words_display">Méga</p>
+                        </article>
+                        <article class="words_card">
+                            <p class="words_display">Youpi</p>
+                        </article>
+                        <article class="words_card">
+                            <p class="words_display">Ah oui</p>
+                        </article>
+                        <article class="words_card">
+                            <p class="words_display"></p>
+                        </article>
+                        <article class="words_card">
+                            <p class="words_display">Super</p>
+                        </article>
+                    </div>
                 
             </div>
         </div>
         <div class="global_right_content">
+        <h2>Last touits</h2>
             <div class="touit_content">
                 <article class="touit">
                     <p class="touit_pseudo">@louisondu37</p>
